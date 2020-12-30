ef1ef3a9fff18f66380de22672a311a1a72cfa4b
Volodymyr Machula <machulav@gmail.com>
2020-12-30 14:36:26 +0200

    Set runner's information to the output earlier

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ async function start() {
   const label = config.generateUniqueLabel();
   const githubRegistrationToken = await gh.getRegistrationToken();
   const ec2InstanceId = await aws.startEc2Instance(label, githubRegistrationToken);
+  setOutput(label, ec2InstanceId);
   await aws.waitForInstanceRunning(ec2InstanceId);
   await gh.waitForRunnerCreated(label);
-  setOutput(label, ec2InstanceId);
 }
 
 async function stop() {
