32f184fdd0f01f1710dcc3595db536ecacb8d61a
lielran <liel.ran.mail@gmail.com>
2020-12-30 16:51:22 +0200

    start docker service

--- src/aws.js
+++ src/aws.js
@@ -9,7 +9,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
     '#!/bin/bash',
     'yum update -y',
     'amazon-linux-extras install -y docker',
-    'chmod +x /usr/local/bin/flyway',
+    'sudo service docker start',
     'exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1',
     'mkdir /actions-runner && cd /actions-runner',
     'curl -O -L https://github.com/actions/runner/releases/download/v2.274.2/actions-runner-linux-x64-2.274.2.tar.gz',
