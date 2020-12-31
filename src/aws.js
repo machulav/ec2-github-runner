f48e1d0abf984b1a079f37326f6687cc7548be96
lielran <liel.ran.mail@gmail.com>
2020-12-31 09:18:30 +0200

    aws ecs docker

--- src/aws.js
+++ src/aws.js
@@ -8,6 +8,8 @@ async function startEc2Instance(label, githubRegistrationToken) {
   const userData = [
     '#!/bin/bash',
     'exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1',
+    'amazon-linux-extras install docker',
+    'service docker start',
     'mkdir /actions-runner && cd /actions-runner',
     'curl -O -L https://github.com/actions/runner/releases/download/v2.274.2/actions-runner-linux-x64-2.274.2.tar.gz',
     'tar xzf ./actions-runner-linux-x64-2.274.2.tar.gz',
