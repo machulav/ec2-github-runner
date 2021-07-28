e57b800123da06fb0a554419c497edad59f64d2d
jpalomaki <jukka@jpalomaki.fi>
2021-07-28 23:55:48 +0300

    Allow user to pass in a custom actions-runner directory which contains pre-installed runner software and scripts

--- src/aws.js
+++ src/aws.js
@@ -5,19 +5,7 @@ const config = require('./config');
 async function startEc2Instance(label, githubRegistrationToken) {
   const ec2 = new AWS.EC2();
 
-  // User data scripts are run as the root user.
-  // Docker and git are necessary for GitHub runner and should be pre-installed on the AMI.
-  const userData = [
-    '#!/bin/bash',
-    'mkdir actions-runner && cd actions-runner',
-    'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
-    'curl -O -L https://github.com/actions/runner/releases/download/v2.278.0/actions-runner-linux-${RUNNER_ARCH}-2.278.0.tar.gz',
-    'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.278.0.tar.gz',
-    'export RUNNER_ALLOW_RUNASROOT=1',
-    'export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1',
-    `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
-    './run.sh',
-  ];
+  const userData = buildUserDataScript(githubRegistrationToken, label);
 
   const params = {
     ImageId: config.input.ec2ImageId,
@@ -39,6 +27,33 @@ async function startEc2Instance(label, githubRegistrationToken) {
   } catch (error) {
     core.error('AWS EC2 instance starting error');
     throw error;
+  }
+}
+
+// User data scripts are run as the root user.
+// Docker and git are necessary for GitHub runner and should be pre-installed on the AMI.
+function buildUserDataScript(githubRegistrationToken, label) {
+  if (config.runnerHomeDir) {
+    return [
+      '#!/bin/bash',
+      `cd "${config.runnerHomeDir}"`,
+      'export RUNNER_ALLOW_RUNASROOT=1',
+      'export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1',
+      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
+      './run.sh',
+    ];
+  } else {
+    return [
+      '#!/bin/bash',
+      'mkdir actions-runner && cd actions-runner',
+      'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
+      'curl -O -L https://github.com/actions/runner/releases/download/v2.278.0/actions-runner-linux-${RUNNER_ARCH}-2.278.0.tar.gz',
+      'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.278.0.tar.gz',
+      'export RUNNER_ALLOW_RUNASROOT=1',
+      'export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1',
+      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
+      './run.sh',
+    ];
   }
 }
 
