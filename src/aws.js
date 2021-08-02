e41d747e41a86aab28ac4cd0008455f78f3baaa3
Arthur Bogin <arthur@viz.ai>
2021-08-03 00:30:03 +0300

    add spot support

--- src/aws.js
+++ src/aws.js
@@ -29,6 +29,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
     SecurityGroupIds: [config.input.securityGroupId],
     IamInstanceProfile: { Name: config.input.iamRoleName },
     TagSpecifications: config.tagSpecifications,
+    InstanceMarketOptions: { MarketType: 'spot' },
   };
 
   try {
