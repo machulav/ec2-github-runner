36b06ef20aade29c6b9cd0b225d14d70cb552e07
Arthur Bogin <arthur@viz.ai>
2021-08-03 21:02:19 +0300

    update path

--- src/aws.js
+++ src/aws.js
@@ -9,8 +9,8 @@ async function startEc2Instance(label, githubRegistrationToken) {
   // Docker and git are necessary for GitHub runner and should be pre-installed on the AMI.
   const userData = [
     '#!/bin/bash',
-    `./config.sh --unattended --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
-    './run.sh',
+    `/home/ubuntu/config.sh --unattended --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
+    '/home/ubuntu/run.sh',
   ];
 
   const params = {
