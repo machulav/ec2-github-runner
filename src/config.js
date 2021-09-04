04ae2485bd82a77cabf8d832f5a0057756aa0bef
Jukka Palomäki <jukka@jpalomaki.fi>
2021-09-04 21:44:09 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -47,7 +47,7 @@ class Config {
     if (this.input.mode === 'start') {
       const isSet = param => param;
       const params = [this.input.ec2ImageId, this.input.ec2InstanceType, this.input.subnetId, this.input.securityGroupId];
-      if (!this.input.ec2LaunchTemplate && !params.every(isSet)) {
+      if (!(this.input.ec2LaunchTemplate || params.every(isSet))) {
         throw new Error(`Not all the required inputs are provided for the 'start' mode`);
       }
     } else if (this.input.mode === 'stop') {
