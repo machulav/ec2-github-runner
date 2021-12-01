99d55738145e577a4a1b7b11d1d47b142b002975
hajapy <hajapy@users.noreply.github.com>
2021-12-01 13:01:35 -0500

    Support trying multiple subnets

--- src/aws.js
+++ src/aws.js
@@ -35,27 +35,32 @@ async function startEc2Instance(label, githubRegistrationToken) {
 
   const userData = buildUserDataScript(githubRegistrationToken, label);
 
-  const params = {
-    ImageId: config.input.ec2ImageId,
-    InstanceType: config.input.ec2InstanceType,
-    MinCount: 1,
-    MaxCount: 1,
-    UserData: Buffer.from(userData.join('\n')).toString('base64'),
-    SubnetId: config.input.subnetId,
-    SecurityGroupIds: [config.input.securityGroupId],
-    IamInstanceProfile: { Name: config.input.iamRoleName },
-    TagSpecifications: config.tagSpecifications,
-  };
+  const subnetId = config.input.subnetId;
+  const subnets = subnetId ? subnetId.replace(/\s/g, '').split(',') : [null];
 
-  try {
-    const result = await ec2.runInstances(params).promise();
-    const ec2InstanceId = result.Instances[0].InstanceId;
-    core.info(`AWS EC2 instance ${ec2InstanceId} is started`);
-    return ec2InstanceId;
-  } catch (error) {
-    core.error('AWS EC2 instance starting error');
-    throw error;
+  for (const subnet of subnets) {
+    const params = {
+      ImageId: config.input.ec2ImageId,
+      InstanceType: config.input.ec2InstanceType,
+      MinCount: 1,
+      MaxCount: 1,
+      UserData: Buffer.from(userData.join('\n')).toString('base64'),
+      SubnetId: subnet,
+      SecurityGroupIds: [config.input.securityGroupId],
+      IamInstanceProfile: { Name: config.input.iamRoleName },
+      TagSpecifications: config.tagSpecifications,
+    };
+    try {
+      const result = await ec2.runInstances(params).promise();
+      const ec2InstanceId = result.Instances[0].InstanceId;
+      core.info(`AWS EC2 instance ${ec2InstanceId} is started`);
+      return ec2InstanceId;
+    } catch (error) {
+      core.warning('AWS EC2 instance starting error');
+      core.warning(error);
+    }
   }
+  core.setFailed(`Failed to launch instance after trying in ${subnets.length} subnets.`);
 }
 
 async function terminateEc2Instance() {
