b05b98a80a3ac0cb6e16f8df2e04ce30424e7c04
Darrell Dominey <83782524+dev-dd@users.noreply.github.com>
2022-01-07 16:06:13 -0330

    Disable Instance MetaData Service v1
    - This improves security and compliance

--- src/aws.js
+++ src/aws.js
@@ -40,6 +40,7 @@ async function startEc2Instance(label, githubRegistrationToken) {
     InstanceType: config.input.ec2InstanceType,
     MinCount: 1,
     MaxCount: 1,
+    MetadataOptions: { HttpTokens: 'required' },
     UserData: Buffer.from(userData.join('\n')).toString('base64'),
     SubnetId: config.input.subnetId,
     SecurityGroupIds: [config.input.securityGroupId],
