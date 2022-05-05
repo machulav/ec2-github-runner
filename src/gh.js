02bc472e9c1655267ca9c23e8a1b9be0aa98ccc2
Zachary Rohrbach <zrohrbach@quickbase.com>
2022-05-05 09:55:48 -0400

    More logging for testing.

--- src/gh.js
+++ src/gh.js
@@ -60,6 +60,8 @@ async function waitForRunnerRegistered(label) {
   core.info(`Waiting ${quietPeriodSeconds}s for the AWS EC2 instance to be registered in GitHub as a new self-hosted runner`);
   await new Promise(r => setTimeout(r, quietPeriodSeconds * 1000));
   core.info(`Checking every ${retryIntervalSeconds}s if the GitHub self-hosted runner is registered`);
+  core.info(`Timeout minutes: ${timeoutMinutes}`);
+  core.info(`Operating system: ${config.input.operatingSystem}`);
 
   return new Promise((resolve, reject) => {
     const interval = setInterval(async () => {
