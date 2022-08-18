7efcfbdf656e18486fd56d0d03c156d6f28df5b1
Simon Labrecque <simon@wegel.ca>
2022-08-18 14:36:47 -0400

    Don't download the runner if the actions-runner directory exists

--- src/aws.js
+++ src/aws.js
@@ -4,28 +4,20 @@ const config = require('./config');
 
 // User data scripts are run as the root user
 function buildUserDataScript(githubRegistrationToken, label) {
-  if (config.input.runnerHomeDir) {
-    // If runner home directory is specified, we expect the actions-runner software (and dependencies)
-    // to be pre-installed in the AMI, so we simply cd into that directory and then start the runner
-    return [
-      '#!/bin/bash',
-      `cd "${config.input.runnerHomeDir}"`,
-      'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
-      './run.sh',
-    ];
-  } else {
-    return [
-      '#!/bin/bash',
-      'mkdir actions-runner && cd actions-runner',
-      'case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
-      'curl -O -L https://github.com/actions/runner/releases/download/v2.286.0/actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
-      'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
-      'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
-      './run.sh',
-    ];
-  }
+  return [
+    '#!/bin/bash',
+    `if [ ! -d "${config.input.runnerHomeDir}" ]; then`,
+    `  mkdir -p "${config.input.runnerHomeDir}" && cd "${config.input.runnerHomeDir}"`,
+    '  case $(uname -m) in aarch64) ARCH="arm64" ;; amd64|x86_64) ARCH="x64" ;; esac && export RUNNER_ARCH=${ARCH}',
+    '  curl -O -L https://github.com/actions/runner/releases/download/v2.286.0/actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
+    '  tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.286.0.tar.gz',
+    '  cd -',
+    'fi',
+    `cd ${config.input.runnerHomeDir}`,
+    'export RUNNER_ALLOW_RUNASROOT=1',
+    `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
+    './run.sh',
+  ];
 }
 
 async function startEc2Instance(label, githubRegistrationToken) {
