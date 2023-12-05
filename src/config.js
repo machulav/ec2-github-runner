5164a677a8b94eb579d8584d1c4bbac2735285b9
Patrick Foh <patrickfoh@gmail.com>
2023-12-05 15:39:45 +0100

    KeyName and EBS Mapping support

--- src/config.js
+++ src/config.js
@@ -15,12 +15,17 @@ class Config {
       iamRoleName: core.getInput('iam-role-name'),
       runnerHomeDir: core.getInput('runner-home-dir'),
       preRunnerScript: core.getInput('pre-runner-script'),
+      blockDeviceMappings: JSON.parse(core.getInput('block-device-mappings')),
+      keyName: core.getInput('key-name'),
     };
 
     const tags = JSON.parse(core.getInput('aws-resource-tags'));
     this.tagSpecifications = null;
     if (tags.length > 0) {
-      this.tagSpecifications = [{ResourceType: 'instance', Tags: tags}, {ResourceType: 'volume', Tags: tags}];
+      this.tagSpecifications = [
+        { ResourceType: 'instance', Tags: tags },
+        { ResourceType: 'volume', Tags: tags },
+      ];
     }
 
     // the values of github.context.repo.owner and github.context.repo.repo are taken from
