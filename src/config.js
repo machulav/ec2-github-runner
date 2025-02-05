dda8cec69cc17ad0e19f8c2b07f18e18d580fc0b
ml-deployment <deployment@milibris.com>
2025-02-05 13:41:33 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -11,11 +11,12 @@ class Config {
       subnetId: core.getInput('subnet-id'),
       securityGroupId: core.getInput('security-group-id'),
       label: core.getInput('label'),
-      ec2InstanceId: core.getInput('ec2-instance-id'),
+      ec2InstanceIds: JSON.parse(core.getInput('ec2-instance-ids')),
       iamRoleName: core.getInput('iam-role-name'),
       runnerHomeDir: core.getInput('runner-home-dir'),
       preRunnerScript: core.getInput('pre-runner-script'),
       marketType: core.getInput('market-type'),
+      runnerCount: core.getInput('runner-count'),
     };
 
     const tags = JSON.parse(core.getInput('aws-resource-tags'));
@@ -56,7 +57,7 @@ class Config {
         throw new Error('Invalid `market-type` input. Allowed values: spot.');
       }
     } else if (this.input.mode === 'stop') {
-      if (!this.input.label || !this.input.ec2InstanceId) {
+      if (!this.input.label || !this.input.ec2InstanceIds) {
         throw new Error(`Not all the required inputs are provided for the 'stop' mode`);
       }
     } else {
