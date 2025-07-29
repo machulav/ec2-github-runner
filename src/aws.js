56d4704c449fa3c3513186e5397643490573ed37
Bruno Vescovi <bruno.vescovi@doofinder.com>
2025-07-29 10:41:06 +0200

    update runner registration

--- src/aws.js
+++ src/aws.js
@@ -3,6 +3,8 @@ const { EC2Client, RunInstancesCommand, TerminateInstancesCommand, waitUntilInst
 const core = require('@actions/core');
 const config = require('./config');
 
+const runnerBasePath = config.input.runInOrgRunner ? `/${config.githubContext.owner}` : `/${config.githubContext.owner}/${config.githubContext.repo}`;
+
 // User data scripts are run as the root user
 function buildUserDataScript(githubRegistrationToken, label) {
   let userData;
@@ -16,7 +18,7 @@ function buildUserDataScript(githubRegistrationToken, label) {
       `echo "${config.input.preRunnerScript}" > pre-runner-script.sh`,
       'source pre-runner-script.sh',
       'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
+      `./config.sh --url https://github.com${runnerBasePath} --token ${githubRegistrationToken} --labels ${label}`,
     ];
   } else {
     userData = [
@@ -29,7 +31,7 @@ function buildUserDataScript(githubRegistrationToken, label) {
       'curl -O -L https://github.com/actions/runner/releases/download/v2.313.0/actions-runner-linux-${RUNNER_ARCH}-2.313.0.tar.gz',
       'tar xzf ./actions-runner-linux-${RUNNER_ARCH}-2.313.0.tar.gz',
       'export RUNNER_ALLOW_RUNASROOT=1',
-      `./config.sh --url https://github.com/${config.githubContext.owner}/${config.githubContext.repo} --token ${githubRegistrationToken} --labels ${label}`,
+      `./config.sh --url https://github.com${runnerBasePath} --token ${githubRegistrationToken} --labels ${label}`,
     ];
   }
   if (config.input.runAsUser) {
